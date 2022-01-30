f55a4f057ba6eb73b4db66bbbf4cfbdaebe377ba
Toto <19580671+tomso11@users.noreply.github.com>
2022-01-30 11:21:32 -0300

    add header and button components

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+
+const Header = ({title}) => {
+  const onClick = () => {
+    console.log("Click")
+  }
+
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Hello' onClick={onClick}/>
+    </header>
+  )
+  
+};
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+// eslint-disable-next-line react/no-typos
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header;

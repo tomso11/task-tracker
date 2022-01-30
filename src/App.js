d90a8c94e306258f6e632075a9a1adffe17445d1
Toto <19580671+tomso11@users.noreply.github.com>
2022-01-30 12:25:26 -0300

    add reminder and conditional toggle

--- src/App.js
+++ src/App.js
@@ -33,10 +33,15 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==id))
 }
 
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header title = {"I'm a title"}/>
-      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask}/> : ('No tasks to show')}
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : ('No tasks to show')}
     </div>
   );
 }
